215bbbfe6732be48344ba500597f36f31c5d820e
NathanLam <125991901+Nathan00yiu@users.noreply.github.com>
2025-11-11 15:48:40 +0800

    Delete models directory

--- models/Table.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-module.exports = mongoose.model('Table', new mongoose.Schema({
-  number: { type: String, unique: true },
-  seats: { type: Number, min: 2, max: 10 },
-  status: { type: String, default: 'available' }
-}));
--- models/User.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-module.exports = mongoose.model('User', new mongoose.Schema({
-  username: { type: String, unique: true },
-  password: String,
-  role: { type: String, default: 'user' }
-}));
